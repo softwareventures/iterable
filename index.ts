1c3e777ebdc2cb80a62338a2eb74496500d0d837
Daniel Cassidy <mail@danielcassidy.me.uk>
2021-03-04 02:52:41 +0000

    feat(iterator): add iterator function

--- index.ts
+++ index.ts
@@ -2,6 +2,10 @@ export function isIterable<T = unknown>(value: Iterable<T> | unknown): value is 
     return typeof value === "object" && value != null && Symbol.iterator in value;
 }
 
+export function iterator<T>(iterable: Iterable<T>): Iterator<T> {
+    return iterable[Symbol.iterator]();
+}
+
 export function first<T>(iterable: Iterable<T>): T | null {
     const iterator = iterable[Symbol.iterator]();
     const {done, value} = iterator.next();
